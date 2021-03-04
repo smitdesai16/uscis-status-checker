66cebebbd3f334c05308ae03d2212c80085b9291
Smit Desai <smit.desai16@gmail.com>
2021-03-04 15:40:56 -0600

    Storing receipt number in shared preferences

--- app/src/main/java/com/smitdesai16/myapplication/MainActivity.java
+++ app/src/main/java/com/smitdesai16/myapplication/MainActivity.java
@@ -1,23 +1,28 @@
 package com.smitdesai16.myapplication;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+import java.util.HashSet;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener, View.OnClickListener {
 
     private static ListView lvStatus;
+    private static EditText etReceiptNumber;
+    private static Button btnAddInStatusChecker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +30,78 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_main);
 
         lvStatus = findViewById(R.id.lvStatus);
+        etReceiptNumber = findViewById(R.id.etReceiptNumber);
+        btnAddInStatusChecker = findViewById(R.id.btnAddInStatusChecker);
 
-        String[] statuses = { "asd", "asd 22", "asd", "asd", "asd", "asd", "asd", "asd", "asd", "asd", "asd", "asd", "asd", "asd", "asd", "asd", "asd", "asd" };
-        ListAdapter listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, statuses);
-        lvStatus.setAdapter(listAdapter);
         lvStatus.setOnItemClickListener(this);
+        lvStatus.setOnItemLongClickListener(this);
+        btnAddInStatusChecker.setOnClickListener(this);
+
+        ReloadListView();
     }
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        // todo open page with code
         String clickedRestaurant = adapterView.getItemAtPosition(i).toString();
         Toast.makeText(this, clickedRestaurant, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnAddInStatusChecker:
+                AddReceiptToSharedPreferences(etReceiptNumber.getText().toString());
+                ReloadListView();
+                etReceiptNumber.setText("");
+                break;
+        }
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+        DeleteReceiptFromSharedPreferences(adapterView.getItemAtPosition(i).toString());
+        ReloadListView();
+        return true;
+    }
+
+    private Set<String> GetReceiptsFromSharedPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences("userSettings", Context.MODE_PRIVATE);
+        return sharedPreferences.getStringSet("receiptNumbers", null);
+    }
+
+    private void AddReceiptToSharedPreferences(String receipt) {
+        if (receipt.equals("")) {
+            return;
+        }
+        SharedPreferences sharedPreferences = getSharedPreferences("userSettings", Context.MODE_PRIVATE);
+        Set<String> receiptNumbers = GetReceiptsFromSharedPreferences();
+        if (receiptNumbers == null) {
+            receiptNumbers = new HashSet<String>();
+        }
+        receiptNumbers.add(receipt);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putStringSet("receiptNumbers", receiptNumbers);
+        editor.apply();
+    }
+
+    private void DeleteReceiptFromSharedPreferences(String receipt) {
+        Set<String> receipts = GetReceiptsFromSharedPreferences();
+        receipts.remove(receipt);
+        SharedPreferences sharedPreferences = getSharedPreferences("userSettings", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putStringSet("receiptNumbers", receipts);
+        editor.apply();
+    }
+
+    private void ReloadListView() {
+        String[] receiptNumberArray = {};
+
+        Set<String> receiptNumbers = GetReceiptsFromSharedPreferences();
+        if (receiptNumbers != null) {
+            receiptNumberArray = receiptNumbers.toArray(new String[0]);
+        }
+        ListAdapter listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, receiptNumberArray);
+        lvStatus.setAdapter(listAdapter);
     }
 }
---
+++ app/src/main/java/com/smitdesai16/myapplication/ReceiptActivity.java
@@ -0,0 +1,14 @@
+package com.smitdesai16.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReceiptActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receipt);
+    }
+}
