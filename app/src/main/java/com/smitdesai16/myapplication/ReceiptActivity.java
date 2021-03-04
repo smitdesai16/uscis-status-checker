d05e5d3a61568b7677140c664985258c99d02e26
Smit Desai <smit.desai16@gmail.com>
2021-03-04 17:46:59 -0600

    Getting data from site

--- app/src/main/java/com/smitdesai16/myapplication/ReceiptActivity.java
+++ app/src/main/java/com/smitdesai16/myapplication/ReceiptActivity.java
@@ -3,9 +3,11 @@ package com.smitdesai16.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.concurrent.ExecutionException;
 
 public class ReceiptActivity extends AppCompatActivity {
 
@@ -25,7 +27,21 @@ public class ReceiptActivity extends AppCompatActivity {
         if(intent.hasExtra("receipt")) {
             // todo make call
             // todo set data in title and description
-            tvTitle.setText(bundle.getString("receipt"));
+            // tvTitle.setText(bundle.getString("receipt"));
+
+            try {
+                String response = new USCISService().execute().get();
+                response = response.substring(response.indexOf("<div class=\"rows text-center\">"));
+                response = response.substring(0, response.indexOf("<div class=\"col-lg-12 case-status-from form3 mt40 clr\">"));
+
+                tvTitle.setText(response.substring(response.indexOf("<h1>") + 4, response.indexOf("</h1>")));
+                tvDescription.setText(response.substring(response.indexOf("<p>") + 3, response.indexOf("</p>")));
+                // tvTitle.setText(response);
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
---
+++ app/src/main/java/com/smitdesai16/myapplication/USCISService.java
@@ -0,0 +1,33 @@
+package com.smitdesai16.myapplication;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class USCISService extends AsyncTask<String, String, String> {
+
+    @Override
+    protected String doInBackground(String... strings) {
+        OkHttpClient client = new OkHttpClient();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://egov.uscis.gov/casestatus/mycasestatus.do").newBuilder();
+        urlBuilder.addQueryParameter("appReceiptNum", "SRC2105450317");
+
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder().url(url).build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return e.toString();
+        }
+    }
+}
